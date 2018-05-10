555d1c69b0423712aaf1e2111da77c0afb36150d
YoussefMaghfour <youssef.maghfour@capgemini.com>
2018-05-10 12:12:47 +0200

    Add 2 functions for phpunit tests

--- tests/ut/AbstractControllerTest.php
+++ tests/ut/AbstractControllerTest.php
@@ -1,1 +1,18 @@
-test
+<?php
+
+/**
+* 
+*/
+class AbstractControllerTest extends PHPUnit_Framework_TestCase
+{
+	
+	public function testStatusCode(){
+    
+        $this->assertEquals(200,200);
+    }
+
+    public function testKeys(){
+    
+         $this->assertEquals('RS256', 'RS256');
+    }
+}
